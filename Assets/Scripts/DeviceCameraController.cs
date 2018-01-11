4fb562a20fcc56615ab3fa191ceb08b18ae13c0e
Jonas <jonas-rah@live.com>
2018-01-11 15:09:55 +0100

    new final final

---
+++ Assets/Scripts/DeviceCameraController.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+ 
+public class DeviceCameraController : MonoBehaviour
+{
+    public RawImage image;
+    public RectTransform imageParent;
+    public AspectRatioFitter imageFitter;
+    private Renderer[] teethRend;
+    public Color teethColor;
+
+    // Device cameras
+    WebCamDevice frontCameraDevice;
+    WebCamDevice backCameraDevice;
+    WebCamDevice activeCameraDevice;
+
+    WebCamTexture frontCameraTexture;
+    WebCamTexture backCameraTexture;
+    WebCamTexture activeCameraTexture;
+
+    // Image rotation
+    Vector3 rotationVector = new Vector3(0f, 0f, 0f);
+
+    // Image uvRect
+    Rect defaultRect = new Rect(0f, 0f, 1f, 1f);
+    Rect fixedRect = new Rect(0f, 1f, 1f, -1f);
+
+    // Image Parent's scale
+    Vector3 defaultScale = new Vector3(1f, 1f, 1f);
+    Vector3 fixedScale = new Vector3(-1f, 1f, 1f);
+    [HideInInspector]public Color avgColor;
+
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        teethRend = new Renderer[GameObject.FindGameObjectsWithTag("Teeth").Length];
+        for (int i = 0; i < teethRend.Length; i++) {
+            teethRend[i] = GameObject.FindGameObjectsWithTag("Teeth")[i].GetComponent<Renderer>();
+        }
+        // Check for device cameras
+        if (WebCamTexture.devices.Length == 0)
+        {
+            Debug.Log("No devices cameras found");
+            return;
+        }
+
+        // Get the device's cameras and create WebCamTextures with them
+        frontCameraDevice = WebCamTexture.devices.Last();
+        backCameraDevice = WebCamTexture.devices.First();
+
+        frontCameraTexture = new WebCamTexture(frontCameraDevice.name);
+        backCameraTexture = new WebCamTexture(backCameraDevice.name, 1920, 1080, 30);
+
+        // Set camera filter modes for a smoother looking image
+        frontCameraTexture.filterMode = FilterMode.Trilinear;
+        backCameraTexture.filterMode = FilterMode.Trilinear;
+
+        // Set the camera to use by default
+        SetActiveCamera(backCameraTexture);
+    }
+
+    // Set the device camera to use and start it
+    public void SetActiveCamera(WebCamTexture cameraToUse)
+    {
+        if (activeCameraTexture != null)
+        {
+            activeCameraTexture.Stop();
+        }
+            
+        activeCameraTexture = cameraToUse;
+        activeCameraDevice = WebCamTexture.devices.FirstOrDefault(device => 
+            device.name == cameraToUse.deviceName);
+
+        image.material.SetTexture("_MainTex", activeCameraTexture);
+        //image.material.mainTexture = activeCameraTexture;
+
+        activeCameraTexture.Play();
+    }
+
+    // Switch between the device's front and back camera
+    /* public void SwitchCamera()
+    {
+        SetActiveCamera(activeCameraTexture.Equals(frontCameraTexture) ? 
+            backCameraTexture : frontCameraTexture);
+    } */
+        
+    // Make adjustments to image every frame to be safe, since Unity isn't 
+    // guaranteed to report correct data as soon as device camera is started
+    void Update()
+    {
+        // Skip making adjustment for incorrect camera data
+        if (activeCameraTexture.width < 100)
+        {
+            //Debug.Log(activeCameraTexture.width);
+            return;
+        }
+
+        // Rotate image to show correct orientation 
+        rotationVector.z = -activeCameraTexture.videoRotationAngle;
+        image.transform.localEulerAngles = rotationVector;
+
+        // Set AspectRatioFitter's ratio
+        float videoRatio = 
+            (float)activeCameraTexture.width / (float)activeCameraTexture.height;
+        imageFitter.aspectRatio = videoRatio;
+
+        // Unflip if vertically flipped
+        image.uvRect = 
+            activeCameraTexture.videoVerticallyMirrored ? fixedRect : defaultRect;
+
+        // Mirror front-facing camera's image horizontally to look more natural
+        imageParent.localScale = 
+            !activeCameraDevice.isFrontFacing ? fixedScale : defaultScale;
+        
+
+        for (int i = 0; i < teethRend.Length; i++) {
+            teethRend[i].material.color = Color.Lerp(teethRend[i].material.color, teethColor, Time.deltaTime);
+        }
+    }
+
+    public Color GetAvgColor() {
+        var colors = activeCameraTexture.GetPixels ((int)(activeCameraTexture.width/2), (int)(activeCameraTexture.height/2), (int)(activeCameraTexture.width * 0.2f), (int)(activeCameraTexture.height * 0.2f)); // no need to look at all of them
+			
+        float r = 0f, g = 0f, b = 0f;
+
+        for (int i = 0; i < colors.Length; i++) {
+            r += colors[i].r;
+            g += colors[i].g;
+            b += colors[i].b;
+        }
+
+        avgColor = new Color(r/colors.Length,g/colors.Length,b/colors.Length);
+
+        //For testing
+        for (int i = 0; i < teethRend.Length; i++) {
+            teethRend[i].material.color = avgColor * 1.5f;
+        }
+
+        return avgColor;
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+using System.IO;
+
+public class Main : MonoBehaviour 
+{
+	[SerializeField] Animator animator;
+	[SerializeField] RawImage cameraFeed;
+	[SerializeField] Camera snapCamera;
+	[SerializeField] GameObject chewModel;
+	[SerializeField] Rigidbody followSphere;
+	private Renderer snapshotModelRenderer;
+	[SerializeField] AudioSource ahhAudioSource;
+	[SerializeField] AudioSource chewAudioSource;
+	[SerializeField] AudioSource reactionAudioSource;
+
+	[System.Serializable]
+	public class FoodColor 
+	{
+		public Color color;
+		public string name;
+		public AudioClip[] clips;
+	}
+
+	[SerializeField] FoodColor[] foodColors;
+
+	void Awake()
+	{
+		StartCoroutine(Sequence());
+	}
+
+	IEnumerator Sequence() 
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		string camDevice = WebCamTexture.devices.First().name;
+		WebCamTexture webCamTexture = new WebCamTexture(camDevice, 1280, 720, 30);
+		webCamTexture.Play();
+
+		while (true)
+		{
+			while (!Input.GetMouseButton(0))
+			{
+				yield return null;
+			}
+
+			reactionAudioSource.Stop();
+
+			animator.SetBool("IsOpen", true);
+
+			while (Input.GetMouseButton(0))
+			{
+				yield return null;
+			}
+
+			chewAudioSource.Play();
+
+			StartCoroutine(Snapshot());
+
+			animator.SetBool("IsOpen", false);
+
+            HSBColor hsb;
+
+            float[] diffs = new float[foodColors.Length];
+			Color avgColor = cameraFeed.gameObject.GetComponent<DeviceCameraController>().GetAvgColor();
+
+            for(int i = 0; i < foodColors.Length; i++)
+            {
+            	diffs[i] = 0f;
+
+            	var hsbTarget = new HSBColor(foodColors[i].color);
+				
+				diffs[i] += Mathf.Abs (HSBColor.Distance (HSBColor.FromColor(avgColor), hsbTarget));
+	            
+            }
+
+            int minIndex = 0;
+            float min = diffs[0];
+
+            for (int i = 1; i < foodColors.Length; i++)
+            {
+//            	Debug.Log(i + " " + diffs[i]);
+            	if (diffs[i] < min)
+            	{
+            		minIndex = i;
+            		min = diffs[i];
+            	}
+            }
+
+			//Debug.Log(foodColors[minIndex].name);
+
+			while (chewAudioSource.isPlaying)
+			{
+				yield return null;
+			}
+
+			Destroy(GameObject.FindGameObjectWithTag("ChewBits"));
+
+            var foodColor = foodColors[minIndex];
+            reactionAudioSource.PlayOneShot(foodColor.clips[Random.Range(0, foodColor.clips.Length)]);
+		}
+	}
+
+	IEnumerator Snapshot() {
+
+		//enable model
+		GameObject chewClone = Instantiate(chewModel, new Vector3(-0.615f, -0.425f, 0.118f), Quaternion.Euler(0,90,0));
+
+		//Get the right camera
+		Camera mainCam = Camera.main;
+		mainCam.enabled = false;
+		snapCamera.enabled = true;
+
+		// wait for graphics to render
+		yield return new WaitForEndOfFrame();
+	
+		// create a texture to pass to encoding
+		Texture2D texture = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
+	
+		// assign new texture to variable
+		snapshotModelRenderer = chewClone.GetComponentInChildren<Renderer>();
+		snapshotModelRenderer.material.SetTexture("_MainTex", texture);
+	
+		// put buffer into texture
+		texture.ReadPixels(new Rect(0,0, Screen.width, Screen.height), 0, 0);
+		texture.Apply();
+		
+		//Turn camera back on
+		mainCam.enabled = true;
+		snapCamera.enabled = false;
+	}
+	
+}
